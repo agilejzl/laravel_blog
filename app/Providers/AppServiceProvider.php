a235e2cbff6f03a22d5aee74b389d657908e399c
Teddy Jiang <agilejzl@gmail.com>
2017-07-20 10:24:37 +0800

    记录SQL查询到日志文件

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,45 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        // \DB::listen(function($sql) {
+        //     dump($sql->bindings);
+        // });
+
+        \DB::listen(
+            function ($sql) {
+                // $sql is an object with the properties:
+                //  sql: The query
+                //  bindings: the sql query variables
+                //  time: The execution time for the query
+                //  connectionName: The name of the connection
+
+                // To save the executed queries to file:
+                // Process the sql and the bindings:
+                foreach ($sql->bindings as $i => $binding) {
+                    if ($binding instanceof \DateTime) {
+                        $sql->bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+                    } else {
+                        if (is_string($binding)) {
+                            $sql->bindings[$i] = "'$binding'";
+                        }
+                    }
+                }
+
+                // Insert bindings into query
+                $query = str_replace(array('%', '?'), array('%%', '%s'), $sql->sql);
+
+                $query = vsprintf($query, $sql->bindings);
+
+                // Save the query to file
+                $logFile = fopen(
+                    storage_path('logs' . DIRECTORY_SEPARATOR . 'query_' . date('Y-m-d') . '.log'),
+                    'a+'
+                );
+                fwrite($logFile, date('Y-m-d H:i:s') . ': ' . $query . PHP_EOL);
+                fclose($logFile);
+            }
+        );
     }
 
     /**
