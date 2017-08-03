e32476b55e43846889af7c22f985f00a890c7aef
Teddy Jiang <agilejzl@gmail.com>
2017-08-03 09:27:39 +0800

    添加上传文件的包支持

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,6 +63,10 @@ return [
             'bucket' => env('AWS_BUCKET'),
         ],
 
+        'media' => [
+            'driver' => 'local',
+            'root'   => public_path().'/media',
+        ],
     ],
 
 ];
